bcdd5d05f5614674a66864cdc1d1f1645ee163c6
hjbyt <sopu00@gmail.com>
2016-01-04 21:21:19 +0200

    -implemented command line arguments parsing -implemented output file support -refactored main, interact

--- main.c
+++ main.c
@@ -2,20 +2,37 @@
  * Main Module
  */
 
+/*
+ * GENERAL TODO:
+ *      - errors need to be propagated backwards ("messaging") instead of calling panic (see: http://moodle.tau.ac.il/mod/forum/discuss.php?d=28021)
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <getopt.h>
 #include "tree.h"
 #include "parse.h"
 #include "calculate.h"
 #include "common.h"
 
 /*
+ * Structs
+ */
+
+typedef struct CommandLineArgs_
+{
+    char* variable_input_file;
+    char* output_file;
+} CommandLineArgs;
+
+/*
  * Function Declarations
  */
 
-bool interact();
+bool parseCommandLineArguments(int argc, char **argv, CommandLineArgs* parsed_args);
+void interact(HashTable variables, FILE* output_file);
 void getLine(char* buffer, unsigned int size);
 
 /*
@@ -31,53 +48,135 @@ void getLine(char* buffer, unsigned int size);
  *      EXIT_SUCCESS if no error was encountered,
  *      EXIT_FAILURE otherwise.
  */
-int main()
+int main(int argc, char **argv)
 {
-    bool should_continue = true;
+    int return_value = EXIT_FAILURE;
+    FILE* variable_input_file = NULL;
+    FILE* output_file = NULL;
 
-    while (should_continue)
-    {
-         should_continue = interact();
+    /* Parse args */
+    CommandLineArgs parsed_args;
+    bool had_parse_error = parseCommandLineArguments(argc, argv, &parsed_args);
+    if (had_parse_error) {
+        printf("Invalid command line arguments, use [-v filename1] [-o filename2]\n");
+        goto end;
+    }
+    if (parsed_args.variable_input_file != NULL
+        && parsed_args.output_file != NULL
+        && strcmp(parsed_args.variable_input_file, parsed_args.output_file) == 0) {
+        printf("Files must be different\n");
+        goto end;
     }
 
-    return EXIT_SUCCESS;
+    /* Open files */
+    if (parsed_args.variable_input_file != NULL) {
+        variable_input_file = fopen(parsed_args.variable_input_file, "r");
+        if (variable_input_file == NULL) {
+            printf("Variable init file doesn't exist or is not readable\n");
+            goto end;
+        }
+    }
+    if (parsed_args.output_file != NULL) {
+        output_file = fopen(parsed_args.output_file, "w");
+        if (output_file == NULL) {
+            printf("Output file is read-only or cannot be created\n");
+            goto end;
+        }
+    }
+
+    /* Parse initial variables */
+    HashTable variables = createHashTable();
+    if (variable_input_file != NULL) {
+        parseVariableInputFile(variable_input_file, variables);
+    }
+
+    /* Interact with user */
+    interact(variables, output_file);
+
+    return_value = EXIT_SUCCESS;
+
+end:
+    if (variable_input_file != NULL) {
+        fclose(variable_input_file);
+    }
+    if (output_file != NULL) {
+        fclose(output_file);
+    }
+    return return_value;
 }
 
 /*
  * Internal Functions
  */
 
-/**
- * Make a single user interaction:
- *      - Get an input line
- *      - Parse and calculate it, and print the result.
- *
- * @return
- *      false iff the quit command was received.
- */
-bool interact()
+/* TODO: doc */
+bool parseCommandLineArguments(int argc, char **argv, CommandLineArgs* parsed_args)
 {
-    char line[MAX_LINE_LENGTH + 1];
-    getLine(line, sizeof(line));
+    VERIFY(parsed_args != NULL);
 
-    Tree* parse_tree = parseLispExpression(line);
+    /* Initialize to defaults */
+    parsed_args->variable_input_file = NULL;
+    parsed_args->output_file = NULL;
 
-    if (isEndCommand(parse_tree)) {
+    /* Parse args */
+    int c;
+    while ((c = getopt(argc, argv, "v:o:")) != -1)
+    {
+        switch (c) {
+            case 'v':
+                parsed_args->variable_input_file =  optarg;
+                break;
+            case 'o':
+                parsed_args->output_file =  optarg;
+                break;
+            case '?':
+                return true;
+            default:
+                panic();
+        }
+    }
+
+    return false;
+}
+
+/* TODO: doc */
+void interact(HashTable variables, FILE* output_file)
+{
+    bool should_print_expression = true;
+    if (output_file == NULL) {
+        output_file = stdout;
+        should_print_expression = false;
+    }
+
+    while (true)
+    {
+        char lisp_expression[MAX_LINE_LENGTH + 1];
+        getLine(lisp_expression, sizeof(lisp_expression));
+
+        Tree* parse_tree = parseLispExpression(lisp_expression);
+
+        if (should_print_expression) {
+            char expression_string[MAX_LINE_LENGTH + 1];
+            expressionToString(parse_tree, expression_string, sizeof(expression_string));
+            fprintf(output_file, "%s\n", expression_string);
+        }
+
+        if (isEndCommand(parse_tree)) {
+            destroyTree(parse_tree);
+            fprintf(output_file, "Exiting...\n");
+            break;
+        }
+
+        /* TODO: use variables in evaluateExpressionTree()*/
+        double result = evaluateExpressionTree(parse_tree);
+        if (isnan((float)result)) {
+            fprintf(output_file, "Invalid Result\n");
+        } else {
+            fprintf(output_file, "res = %.2f\n", result);
+        }
+
         destroyTree(parse_tree);
-        printf("Exiting...\n");
-        return false;
     }
-
-    double result = evaluateExpressionTree(parse_tree);
-    destroyTree(parse_tree);
-
-    if (isnan((float)result)) {
-        printf("Invalid Result\n");
-    } else {
-        printf("res = %.2f\n", result);
-    }
-
-    return true;
 }
 
 /**
