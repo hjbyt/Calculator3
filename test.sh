1ad13a6bd7a71a3d01bf376f7888e46c72c31faf
oalightning <omeramosbarak@gmail.com>
2016-01-06 14:38:03 +0000

    Added support for min and max in the java and antlr

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 for i in {1..5}
 do 
     mkdir -p out/$i
-    java SP.SPCalculatorMain -i tests/$i/input$i.in -e out/$i/test$i.err | ./ex3 > out/$i/test$i.out
+    java SP.SPCalculatorMain -i tests/$i/input$i.in -e out/$i/test$i.err | ./SPCalculator > out/$i/test$i.out
     diff out/$i/test$i.out tests/$i/expected$i.out > out/$i/test$i.out.diff || echo There is a problem with test $i
     diff out/$i/test$i.err tests/$i/expected$i.err > out/$i/test$i.err.diff || echo There is a problem with test $i
 done
